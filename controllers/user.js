ca4380700bee340f771861b74338fa46525d6ab9
sendyakbar <msendyakbars@gmail.com>
2018-10-25 14:32:12 +0700

    crud user done

---
+++ controllers/user.js
@@ -0,0 +1,79 @@
+const Model = require('../models')
+
+class ControllerUser {
+
+    static renderFormRegister(req, res) {
+        Model.User.findAll()
+            .then(function(data) {
+                res.render('pages/user-register.ejs')
+            })
+            .catch(function(err) {
+                res.send(err)
+            })
+    }
+
+    static addData(req, res) {
+        let user = new Model.User({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password,
+            role: "customer"
+        })
+        user.save()
+            .then(function(user) {
+                res.send('added')
+            })
+            .catch(function(err) {
+                res.send(err)
+            })
+    }
+
+    static renderProfileUser(req, res) {
+        Model.User.findById(req.params.id)
+            .then(function(data) {
+                res.render('pages/user-profile', {data: data})
+            })
+            .catch(function(err) {
+                res.send(err)
+            })
+    }
+
+    static renderEditProfileForm(req, res) {
+        Model.User.findById(req.params.id)
+            .then(function(data) {
+                res.render('pages/user-profile-edit.ejs', {data: data})
+            })
+            .catch(function(err) {
+                res.send(err)
+            })
+    }
+
+    static editUserProfile(req, res) {
+        Model.User.update({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password
+        }, {where: {id: req.params.id}})
+        .then(function(data) {
+            res.send('edited')
+            // res.redirect('/user/profile/edit/:id')
+        })
+        .catch(function(err) {
+            res.send(err)
+        })
+    }
+
+    static deleteUser(req, res) {
+        Model.User.destroy({where: {id: req.params.id}})
+            .then(function(data) {
+                res.send('deleted')
+            })
+            .catch(function(err) {
+                res.send(err)
+            })
+    }
+}
+
+module.exports = ControllerUser
