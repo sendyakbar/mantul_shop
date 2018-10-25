7e2262b6d41f6434f185df055d20a35851cd619d
vinutama <muhammadkhevin@gmail.com>
2018-10-25 17:36:31 +0700

    refactoring product for customer

--- routes/product.js
+++ routes/product.js
@@ -1,22 +1,29 @@
 const routerProduct = require('express').Router()
 const ControllerProduct = require('../controllers/product')
+const ControllerProductCustomer = require('../controllers/product-customer')
 
-//GET PAGE PRODUCTS
+//GET PAGE PRODUCTS ADMIN
 routerProduct.get('/product', ControllerProduct.renderData)
 
-//GET ADD PRODUCT PAGE
+//GET ADD PRODUCT PAGE ADMIN
 routerProduct.get('/product/add', ControllerProduct.renderDataAdd)
 
-//POST ADD PRODUCT
+//POST ADD PRODUCT ADMIN
 routerProduct.post('/product/add', ControllerProduct.addData)
 
-//GET DELETE PRODUCT
+//GET DELETE PRODUCT ADMIN
 routerProduct.get('/product/delete/:id', ControllerProduct.deleteData)
 
-//GET EDIT PRODUCT
+//GET EDIT PRODUCT ADMIN
 routerProduct.get('/product/edit/:id', ControllerProduct.renderEditData)
 
-//POST  EDIT PRODUCT
+//POST  EDIT PRODUCT ADMIN
 routerProduct.post('/product/edit/:id', ControllerProduct.postEditData)
+
+//GET PAGE PRODUCT CUSTOMER
+routerProduct.get('/product/buy', ControllerProductCustomer.renderData)
+
+
+
 
 module.exports = routerProduct
