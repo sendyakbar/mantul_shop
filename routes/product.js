f9af4679594a19ff4efbcca15739c3dd44ef5ce5
vinutama <muhammadkhevin@gmail.com>
2018-10-25 15:53:37 +0700

    add CRUD product and tag

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,22 @@
 const routerProduct = require('express').Router()
+const ControllerProduct = require('../controllers/product')
+
+//GET PAGE PRODUCTS
+routerProduct.get('/product', ControllerProduct.renderData)
+
+//GET ADD PRODUCT PAGE
+routerProduct.get('/product/add', ControllerProduct.renderDataAdd)
+
+//POST ADD PRODUCT
+routerProduct.post('/product/add', ControllerProduct.addData)
+
+//GET DELETE PRODUCT
+routerProduct.get('/product/delete/:id', ControllerProduct.deleteData)
+
+//GET EDIT PRODUCT
+routerProduct.get('/product/edit/:id', ControllerProduct.renderEditData)
+
+//POST  EDIT PRODUCT
+routerProduct.post('/product/edit/:id', ControllerProduct.postEditData)
 
 module.exports = routerProduct
--- routes/tag.js
+++ routes/tag.js
@@ -1,3 +1,22 @@
 const routerTag = require('express').Router()
+const controllerTag = require('../controllers/tag')
+
+//RENDER DATA TAG PAGE
+routerTag.get('/tags', controllerTag.renderData)
+
+//RENDER DATA ADD TAG PAGE
+routerTag.get('/tags/add', controllerTag.renderAddData)
+
+//POST DATA TAG PAGE
+routerTag.post('/tags/add', controllerTag.addData)
+
+//DELETE PRODUCT
+routerTag.get('/tags/delete/:id', controllerTag.deleteData)
+
+//RENDER DATA EDIT TAG PAGE
+routerTag.get('/tags/edit/:id', controllerTag.renderEditData)
+
+//POST DATA EDIT TAG PAGE
+routerTag.post('/tags/edit/:id', controllerTag.postEditData)
 
 module.exports = routerTag
